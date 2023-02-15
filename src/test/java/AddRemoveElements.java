54473604c83db6e1fe7c439ba255135e2832eb3f
Smilex <sm1l3x@mail.ru>
2023-02-15 20:18:08 +0300

    Initial commit

---
+++ src/test/java/AddRemoveElements.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class AddRemoveElements {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("web-driver.chrome.driver", "src/test/resources/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown(){
+        driver.quit();
+    }
+}
