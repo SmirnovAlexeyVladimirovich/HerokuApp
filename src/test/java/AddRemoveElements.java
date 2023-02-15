a3102f5f5f4048c82bc7aae33c7e828910642163
Smilex <sm1l3x@mail.ru>
2023-02-15 21:29:58 +0300

    Added Base Test class

--- src/test/java/AddRemoveElements.java
+++ src/test/java/AddRemoveElements.java
@@ -1,29 +1,17 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
-
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
 
-public class AddRemoveElements {
+public class AddRemoveElements extends BaseTest {
 
-    WebDriver driver;
-
-    @BeforeMethod
-    public void setUp() {
-        System.setProperty("web-driver.chrome.driver", "src/test/resources/chromedriver");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    @Test
+    public void addRemove(){
+        driver.get("http://the-internet.herokuapp.com/add_remove_elements/");
+        driver.findElement(By.xpath("//button[text()='Add Element']")).click();
+        driver.findElement(By.xpath("//button[text()='Add Element']")).click();
+        driver.findElements(By.xpath("//button[text()='Delete']")).get(1).click();
+        int numberOfElements = driver.findElements(By.xpath("//button[text()='Delete']")).size();
+        assertEquals(numberOfElements, 1, "Number of DELETE buttons is wrong");
     }
 
-    @AfterMethod(alwaysRun = true)
-    public void tearDown(){
-        driver.quit();
-    }
 }
